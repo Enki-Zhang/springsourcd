b74ccccc57aa5bba3aa5a25330f4b9d3381a930c
Enki-Zhang <17855378518@163.com>
2023-08-01 10:46:41 +0800

    init

---
+++ springcode/src/main/java/com/enki/springcode/SpringcodeApplication.java
@@ -0,0 +1,14 @@
+package com.enki.springcode;
+
+import com.enki.springcode.demos.web.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcodeApplication.class, args);
+    }
+
+}
---
+++ springcode/src/test/java/com/enki/springcode/SpringcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enki.springcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
